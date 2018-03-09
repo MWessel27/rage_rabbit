acb6f7e484e2eae81d298bf26922027f0e9bde4d
MWessel27 <mwessel27@gmail.com>
2018-03-09 16:05:41 -0500

    Add view controllers and list table view for products

---
+++ rage_rabbit/ListViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ListViewController.swift
+//  rage_rabbit
+//
+//  Created by Mikal Wessel on 3/9/18.
+//  Copyright © 2018 persever. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var inptBox: UITextField!
+    @IBOutlet weak var enterBtn: UIButton!
+    @IBOutlet weak var prodTbl: UITableView!
+    
+    var products: [String] = [];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        prodTbl.dataSource = self as? UITableViewDataSource;
+        
+        if(products.count == 0){
+            prodTbl.isHidden = true;
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnClick(_ sender: Any) {
+        let prodName = self.inptBox.text;
+        products.append(prodName!);
+        
+        prodTbl.beginUpdates();
+        prodTbl.insertRows(at: [
+            NSIndexPath(row: products.count-1, section: 0) as IndexPath
+            ], with: .automatic);
+        prodTbl.endUpdates();
+        prodTbl.isHidden = false;
+        self.inptBox.text = "";
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        
+        cell.textLabel?.textAlignment = .center;
+        
+        let text = products[indexPath.row];
+        
+        cell.textLabel?.text = text;
+        
+        return cell;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
