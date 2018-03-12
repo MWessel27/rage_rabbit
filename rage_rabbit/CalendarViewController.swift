51a69c42fca077189532e4e6e4c8e55a2c2ab9f3
MWessel27 <mwessel27@gmail.com>
2018-03-12 14:13:00 -0400

    Click on a day in the calendar, and the date will transcend to the day on the product day list

--- rage_rabbit/CalendarViewController.swift
+++ rage_rabbit/CalendarViewController.swift
@@ -14,23 +14,49 @@ class CalendarViewController: UIViewController, FSCalendarDataSource, FSCalendar
     
     var datesWithEvent = ["2018-03-03", "2018-03-06", "2018-03-12", "2018-03-25"]
     
-    fileprivate lazy var dateFormatter2: DateFormatter = {
+    var dateString = String()
+    
+    fileprivate lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
+    
+    fileprivate lazy var hrDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM dd, yyyy"
         return formatter
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
-        calendar.dataSource = self as? FSCalendarDataSource
-        calendar.delegate = self as? FSCalendarDelegate
+        calendar.dataSource = self as FSCalendarDataSource
+        calendar.delegate = self as FSCalendarDelegate
         self.calendar = calendar
         // Do any additional setup after loading the view.
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let today = self.hrDateFormatter.string(from: Date())
+        let dayController = segue.destination as! ViewController
+        dayController.dayStr = "Today"
+        if(today != dateString) {
+            dayController.dayStr = dateString
+        }
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        self.dateString = self.hrDateFormatter.string(from: date)
+        if monthPosition == .previous || monthPosition == .next {
+            calendar.setCurrentPage(date, animated: true)
+        }
+        performSegue(withIdentifier: "todaySegue", sender: self)
+    }
+    
     func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
-        let dateString = self.dateFormatter2.string(from: date)
+        let dateString = self.dateFormatter.string(from: date)
         if self.datesWithEvent.contains(dateString) {
             return 1
         }
@@ -38,7 +64,7 @@ class CalendarViewController: UIViewController, FSCalendarDataSource, FSCalendar
     }
     
     func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, eventColorFor date: Date) -> UIColor? {
-        let dateString = self.dateFormatter2.string(from: date)
+        let dateString = self.dateFormatter.string(from: date)
         if self.datesWithEvent.contains(dateString) {
             return UIColor.purple
         }
--- rage_rabbit/ViewController.swift
+++ rage_rabbit/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var dayLbl: UILabel!
+    
+    var dayStr = String("Today")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        dayLbl.text = dayStr
+        if(dayLbl.text == "") {
+            dayLbl.text = "Today"
+        }
     }
 
     override func didReceiveMemoryWarning() {
