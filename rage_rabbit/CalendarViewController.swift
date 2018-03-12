ac6592f135820907c7cc549bf28141f3ae91369f
MWessel27 <mwessel27@gmail.com>
2018-03-12 11:42:55 -0400

    Calendar updates and fake events

--- rage_rabbit/CalendarViewController.swift
+++ rage_rabbit/CalendarViewController.swift
@@ -11,15 +11,38 @@ import UIKit
 class CalendarViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
     
     fileprivate weak var calendar: FSCalendar!
+    
+    var datesWithEvent = ["2018-03-03", "2018-03-06", "2018-03-12", "2018-03-25"]
+    
+    fileprivate lazy var dateFormatter2: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let calendar = FSCalendar(frame: CGRect(x: 0, y: 0, width: 320, height: 300))
         calendar.dataSource = self as? FSCalendarDataSource
         calendar.delegate = self as? FSCalendarDelegate
-        // view.addSubview(calendar)
         self.calendar = calendar
         // Do any additional setup after loading the view.
+    }
+    
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        let dateString = self.dateFormatter2.string(from: date)
+        if self.datesWithEvent.contains(dateString) {
+            return 1
+        }
+        return 0
+    }
+    
+    func calendar(_ calendar: FSCalendar, appearance: FSCalendarAppearance, eventColorFor date: Date) -> UIColor? {
+        let dateString = self.dateFormatter2.string(from: date)
+        if self.datesWithEvent.contains(dateString) {
+            return UIColor.purple
+        }
+        return nil
     }
 
     override func didReceiveMemoryWarning() {
